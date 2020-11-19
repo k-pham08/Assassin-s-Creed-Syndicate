9ec52164ec9502a62ad50dad2402ec75289a75a1
Khoa Pham <j.dangkhoa.118@gmail.com>
2020-11-20 00:15:51 +0700

    no dropdown

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function onprice(){
+    var edition = document.getElementById('edition');
+    if(edition.value == 'standard'){
+        document.getElementById('bill').value = 39.99 + '$';
+    }
+    else if(edition.value == 'gold'){
+        document.getElementById('bill').value = 69.99 + '$';
+    }
+    else if(edition.value == 'deluxe'){
+        document.getElementById('bill').value = 50.99 + '$';
+    }
+    else{
+        document.getElementById('bill').value = 29.99 + '$'
+    }
+}
+
